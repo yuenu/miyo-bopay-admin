11d3f0dfc5fde5bae80d5f0ea9b6bfb10ea29a39
Jessica <>
2021-07-08 17:57:56 +0800

    add: ws ping

--- src/utils/ws.js
+++ src/utils/ws.js
@@ -2,25 +2,28 @@ import Notification from "@/components/factory/NotifFactory";
 import { wsType } from "@/utils/enum";
 const wsUrl = process.env.REACT_APP_WS_URL;
 export var socket = null;
-
+const handlePinPerSecond = () => {
+  socket.send(JSON.stringify({ type: "ping" }));
+};
 export const connectSocket = () => {
   socket = new WebSocket(wsUrl);
   socket.onopen = function () {
-    console.log("websocket connected!!", socket.readyState);
+    setInterval(handlePinPerSecond, 10 * 1000);
   };
   socket.onmessage = function (msg) {
     const res = JSON.parse(msg.data);
-    console.log(res);
-    Notification({
-      title: wsType[res.type],
-      message: "",
-    });
+    res.type !== "pong" &&
+      Notification({
+        title: wsType[res.type] || "ws",
+        message: "",
+      });
   };
   socket.onerror = function (err) {
     console.log("error", err);
   };
   socket.onclose = function () {
     // connectSocket();
+    clearInterval(handlePinPerSecond);
   };
 };
 export const sendSocketMessage = msg => {
